c85d4088c338e71ab061a54e4416b16d44c3f5c4
ViniGSouza <viniciusgdev@gmail.com>
2025-03-27 21:45:10 -0300

    feat: adicionado bottom tab navigator

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -5,7 +5,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {AuthStack, AppStack} from '@routes';
 
 export function Router() {
-  const authenticated = false;
+  const authenticated = true;
 
   return (
     <NavigationContainer>
