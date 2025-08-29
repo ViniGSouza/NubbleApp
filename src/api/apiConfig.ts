8728d6753bd34855687d522614ee47cf799fc138
ViniGSouza <viniciusgdev@gmail.com>
2025-08-29 20:32:41 -0300

    feat: criacao do dominio post-comment e hook de paginacao

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333',
   headers: {
     Authorization:
-      'Bearer MQ.vJ6iXNjpZ1JctX6PdBPThWtTcsMqhDl4dIgYXfaYChHr8Ui8FlmcPOkccCjJ',
+      'Bearer MQ.uVaQ-55JtqCdVIqma4LvQrWYXNcSVthmiwZvvc3H48NaLGpdjJK3A0IJ63bu',
   },
 });
